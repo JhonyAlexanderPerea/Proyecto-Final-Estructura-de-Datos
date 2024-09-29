1d9bc2f8c0dac9a193568efd67c556b97b20e6f4
JhonyAlexanderPerea <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2024-09-28 19:01:53 -0500

    Update
    Update viaje.java

--- src/main/java/Model/Viaje.java
+++ src/main/java/Model/Viaje.java
@@ -1,4 +1,10 @@
 package Model;
 
+import java.util.Date;
+
 public class Viaje {
+    private static final String ORIGEN = "CDMX";
+    private static String destino;
+    private Date duracion;
+    private Date horaSalida;
 }
