d0c75f1951ce0bfb46faaec530c18abde42ca2fb
FabianValencia23 <127154183+FabestValencia@users.noreply.github.com>
2024-10-12 20:10:20 -0500

    Utils
    Se añadieron archivos utilitarios al proyecto

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.example.proyectofinalestructuradedatos {
     requires javafx.controls;
     requires javafx.fxml;
     requires static lombok;
+    requires java.desktop;
+    requires java.logging;
 
 
 }
