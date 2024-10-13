ecea3603ee836016a368b019d4dfc43201c30ecd
FabianValencia23 <127154183+FabestValencia@users.noreply.github.com>
2024-10-12 20:30:07 -0500

    Se añadieron clases necesarias
    Se añadieron clases necesarias para comenzar la fase de desarrollo

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -1,4 +1,11 @@
 package App;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
