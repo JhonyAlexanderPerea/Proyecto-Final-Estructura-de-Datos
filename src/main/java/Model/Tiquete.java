ff4bdae8d21efa71aae3fcfec9e3b9c3fde9af27
FabianValencia23 <127154183+FabestValencia@users.noreply.github.com>
2024-10-30 14:18:02 -0500

    correoUtil

--- src/main/java/Model/Tiquete.java
+++ src/main/java/Model/Tiquete.java
@@ -6,9 +6,15 @@ import java.util.Date;
 @Data
 
 public  class Tiquete {
+    private Viaje viaje;
     private String id;
     private Date fechaInicio;
     private Date fechaRegreso;
     private int cantPersonas;
     private int costo;
+
+    public double calcularCostoTotal() {
+        double tasaAdicional = viaje.isEsInternacional() ? 0.97 : 0.8;
+        return costo * (1 + tasaAdicional / 100);
+    }
 }
