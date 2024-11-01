be2efacac31216ed6ee0552b83462a9e5302d345
ingemaicol <michaels.lopezg@uqvirtual.edu.co>
2024-11-01 14:24:54 -0500

    VentanaCPrincipal.fxml agregada 1

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -14,9 +14,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/View/VentanaCompra.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/VentanaPrincipal.fxml"));
         primaryStage.setTitle("Uniquindio Airlines");
         primaryStage.setScene(new Scene(root));
-        primaryStage.show();//Prueba2
+        primaryStage.show(); //Prueba2
     }
 }
--- src/main/java/controller/ControllerVentanaPrincipal.java
+++ src/main/java/controller/ControllerVentanaPrincipal.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
+
 public class ControllerVentanaPrincipal {
+
+    @FXML
+    private void abrirVentanaTripulante(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/VentanaTripulante.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Ventana Tripulante");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            Stage stagePrincipal = (Stage) ((Parent) event.getSource()).getScene().getWindow();
+            stagePrincipal.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
